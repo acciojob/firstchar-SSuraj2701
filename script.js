70a42fe1ea523c632b731edac8faf43cd52b3e39
Suraj Singh <suraj.job.2002@gmail.com>
2025-11-13 16:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
   // your code here
+	let a=text.trim();
+	if (a===""){
+		return ""
+	}
+	else{
+		return a[0]
+	}
+	
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
